228bfdb49cd579c2dbd0b022f19242b1e4de0745
Ian Chen <barkatthetree@gmail.com>
2023-06-27 07:07:52 +0000

    chore: fix colors and add github button

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,21 @@ function App() {
   return (
     <EditorContextProvider>
       <div className="banner">
-        <a href="https://postman.gov.sg">
+        <a target="_blank" href="https://postman.gov.sg" rel="noreferrer">
           <img height="40px" src="/postman.svg"></img>
         </a>
-        <p>unofficial email editor</p>
+        <p>
+          Email HTML Editor <span className="beta-tag">beta</span>
+        </p>
       </div>
+      <a
+        href="https://github.com/opengovsg/postman-editor"
+        target="_blank"
+        className="github-link"
+        rel="noreferrer"
+      >
+        <img height="25px" src="/github.png"></img>
+      </a>
       <div className="row">
         <div className="card">
           <RichTextEditor />
